e0dd0a388eb01df6a023d7328ae64b8de58fa29c
Gabriel Santos <gabrielsantoscamargo8@gmail.com>
2024-07-29 13:04:32 -0300

    Exercício de OOP com menu
    Bem simples apenas para relembrar sintaxe e lógica

---
+++ Orientação-Objetos/Exercícios/exe01- OOP/exe01- OOP/Produto.cs
@@ -0,0 +1,35 @@
+﻿
+namespace exe01__OOP
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public double Preco { get; set; }
+        public int quantidade { get; set; }
+
+        public Produto(int id, string nome, double preco, int quantidade)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        public double valorTotalEmEstoque()
+        {
+            return this.Preco * this.quantidade;
+        }
+
+        public void adicionarProdutos(int quantidade)
+        {
+            this.quantidade += quantidade;
+        }
+
+        public void removerProdutos(int quantidade)
+        {
+            this.quantidade -= quantidade;
+        }
+
+    }
+}
---
+++ Orientação-Objetos/Exercícios/exe01- OOP/exe01- OOP/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace exe01__OOP
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("SISTEMA DE ESTOQUE");
+            int opcao = 0;
+            
+            List<Produto> produtos = new List<Produto>();
+
+            int id = 1;
+
+            do
+            {
+                Console.WriteLine("Selecione a opção que voce deseja realizar");
+                Console.WriteLine("[1] Adicionar Produto");
+                Console.WriteLine("[2] Consultar Valor de estoque de um produto");
+                Console.WriteLine("[3] Adicionar Produtos");
+                Console.WriteLine("[4] Remover Produtos");
+                Console.WriteLine("[5] Sair do Programa");
+
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao){
+
+                    case 1:
+                        Console.WriteLine("Digite o nome do produto");
+                        string nome = Console.ReadLine();
+
+                        Console.WriteLine("Digite o Preco do produto");
+                        double preco = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Digite a quantidade em estoque do produto");
+                        int quantide = int.Parse(Console.ReadLine());
+
+                        Produto produto = new Produto(id, nome, preco, quantide);
+
+                        produtos.Add(produto);
+                        
+                        id++;
+
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine("Informe ID do produto: ");
+
+                        int buscaId = int.Parse(Console.ReadLine());
+
+                        Produto produtoFind = produtos.Find(produto => produto.Id == buscaId);
+
+                        if(produtoFind == null)
+                        {
+                            Console.WriteLine("Produto não encontrado!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Valor em estoque: " + produtoFind.valorTotalEmEstoque());
+                        }
+
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("Informe ID do produto: ");
+
+                        buscaId = int.Parse(Console.ReadLine());
+
+                        produtoFind = produtos.Find(produto => produto.Id == buscaId);
+
+                        if (produtoFind == null)
+                        {
+                            Console.WriteLine("Produto não encontrado!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Digite o valor que deseja adicionar no estoque:");
+                            int quantidadeAdd = int.Parse(Console.ReadLine());
+
+                            produtoFind.adicionarProdutos(quantidadeAdd);
+
+                            Console.WriteLine("Quantidade em estoque atual: " + produtoFind.quantidade);
+                        }
+
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine("Informe ID do produto: ");
+
+                        buscaId = int.Parse(Console.ReadLine());
+
+                        produtoFind = produtos.Find(produto => produto.Id == buscaId);
+
+                        if (produtoFind == null)
+                        {
+                            Console.WriteLine("Produto não encontrado!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Digite o valor que deseja Retirar no estoque:");
+                            int quantidadeSub = int.Parse(Console.ReadLine());
+
+                            produtoFind.adicionarProdutos(quantidadeSub);
+
+                            Console.WriteLine("Quantidade em estoque atual: " + produtoFind.quantidade);
+                        }
+
+                        break;
+                    case 5:
+                        Console.WriteLine("Saindo...");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Caracter Inválido!! Digite uma opção válida");
+                        break;
+                }
+            }while (opcao != 5) ;
+        }
+
+    }
+}
