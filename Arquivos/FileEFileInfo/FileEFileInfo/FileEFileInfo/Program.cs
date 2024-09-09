8a27af5967afaa0813ad545323e47c265ef1a401
Gabriel Santos <gabrielsantoscamargo8@gmail.com>
2024-09-09 10:15:42 -0300

    Manipulacao de arquivos

---
+++ Arquivos/FileEFileInfo/FileEFileInfo/FileEFileInfo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using System.IO;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\gabri\Documents\Programacao\file1.txt";
+
+            string targetPath = @"C:\Users\gabri\Documents\Programacao\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ocerreu");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
